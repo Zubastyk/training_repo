52d669f5c188b0fad97aa262024e73a679d1a4b6
Zubastyk <122399636+Zubastyk@users.noreply.github.com>
2023-02-01 21:08:13 +0300

    test.py
    Листинг 16.1 Содержимое файла test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+#-*- coing: utf-8-*-
+import os, sys
+print("%-25s%s" % ("Файл:", os.path.abspath(__file__)))
+print("%-25s%s" % ("Текущий рабочй каталог:", os.getcwd()))
+print("%-25s%s" % ("Каталог для импорта:", sys.path[0]))
+print("%-25s%s" % ("Путь файлу:", os.path.abspath("file.txt")))
+print("-"*40)
+import folder1.module1 as m
+m.get_cwd()
