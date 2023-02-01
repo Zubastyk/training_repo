93e23a78874ecc827c235c6e531f678b449db77b
Zubastyk <122399636+Zubastyk@users.noreply.github.com>
2023-02-01 21:13:53 +0300

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,4 +1,7 @@
 #-*- coding: utf-8 -*-
 import os, sys
 def get_cwd():
-  print("%-25s%s" % ("Файл:", 
+    print("%-25s%s" % ("Файл:", os.path.abspath(__file__)))
+    print("%-25s%s" % ("Текущий рабочй каталог:", os.getcwd()))
+    print("%-25s%s" % ("Каталог для импорта:", sys.path[0]))
+    print("%-25s%s" % ("Путь файлу:", os.path.abspath("file.txt")))
